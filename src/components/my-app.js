90cfd8d9edc928afaaada93346d7e25545fb418d
BURCHFIELD, JOHN <john.burchfield2@bbva.com>
2019-09-10 16:11:07 -0500

    feat(js): added new font family and removed toolbar

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -19,7 +19,6 @@ import { updateMetadata } from 'pwa-helpers/metadata.js';
 import '@polymer/app-layout/app-drawer/app-drawer.js';
 import '@polymer/app-layout/app-header/app-header.js';
 import '@polymer/app-layout/app-scroll-effects/effects/waterfall.js';
-import '@polymer/app-layout/app-toolbar/app-toolbar.js';
 import '@polymer/paper-card/paper-card.js';
 import { menuIcon } from './my-icons.js';
 import './snack-bar.js';
@@ -45,6 +44,7 @@ class MyApp extends LitElement {
       css`
         :host {
           display: block;
+          font-family: 'Muli';
 
           --app-drawer-width: 340px;
 
@@ -75,52 +75,13 @@ class MyApp extends LitElement {
           border-bottom: 1px solid #eee;
         }
 
-        .toolbar-top {
-          background-color: var(--app-header-background-color);
-        }
-
         .slide-bottom {
           -webkit-animation: slide-in-fwd-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 	        animation: slide-in-fwd-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
         }
 
-        [main-title] {
-          font-family: 'Muli';
-          font-size: 30px;
-          /* In the narrow layout, the toolbar is offset by the width of the
-          drawer button, and the text looks not centered. Add a padding to
-          match that button */
-          padding-right: 44px;
-        }
-
-        .toolbar-list {
-          display: none;
-        }
-
         #mobile-drawer {
           display: none;
-        }
-
-        .toolbar-list > a {
-          display: inline-block;
-          color: var(--app-header-text-color);
-          text-decoration: none;
-          line-height: 30px;
-          padding: 4px 24px;
-        }
-
-        .toolbar-list > a[selected] {
-          color: var(--app-primary-color);
-          border-bottom: 4px solid var(--app-header-selected-color);
-        }
-
-        .menu-btn {
-          background: none;
-          border: none;
-          fill: var(--app-header-text-color);
-          cursor: pointer;
-          height: 44px;
-          width: 44px;
         }
 
         .drawer-list {
@@ -165,7 +126,6 @@ class MyApp extends LitElement {
         }
 
         .main-content {
-          padding-top: 64px;
           min-height: 100vh;
         }
 
@@ -195,12 +155,6 @@ class MyApp extends LitElement {
   render() {
     // Anything that's related to rendering should be done in here.
     return html`
-      <!-- Header -->
-      <app-header condenses reveals effects="waterfall">
-        <app-toolbar class="toolbar-top">
-          <div main-title>${this.appTitle}</div>
-        </app-toolbar>
-      </app-header>
 
       <!-- Drawer content -->
       <app-drawer
@@ -291,7 +245,7 @@ class MyApp extends LitElement {
   _getAppDrawer() {
     return html`
       <nav class="drawer-list">
-          <h2>Schools</h2>
+          <h2>${this.appTitle}</h2>
           ${this._schools.map(school =>
           html`<a id="${school.school_id}" @click="${this._schoolClicked}" ?selected="${this._isSchoolSelected(school.school_id)}">
             ${school.school_ds}
