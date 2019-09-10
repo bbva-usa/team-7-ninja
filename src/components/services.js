1f1c9e588593092f1f545c11cbb7ab9af02917e4
BURCHFIELD, JOHN <john.burchfield2@bbva.com>
2019-09-10 10:33:57 -0500

    feat(all): added eslint, services

---
+++ src/components/services.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+
+import { Fetcher } from './services-fetcher';
+
+export class Services {
+  /** ---- STATIC MEMBERS - RETURN INSTANCE OF FETCHER ---- */
+
+  static get routes() {
+    return new Fetcher('routes');
+  }
+}
